15523c59756b6f8121a88c954b7b75559dcc97f0
Elena Alexandrova <helex07@gmail.com>
2019-01-16 12:41:47 +1000

    edited vignettes

--- vignettes/farspack_details.R
+++ vignettes/farspack_details.R
@@ -1,22 +1,22 @@
 ## ----get_my_pack---------------------------------------------------------
 library(farspack)
 
-## ---- setup, include=FALSE, echo=FALSE-----------------------------------
-require("knitr")
-opts_knit$set(root.dir = "~/Desktop/R program spec/R prog Course 3/farspack/inst/extdata")
-
 ## ---- name, echo = TRUE, eval = TRUE-------------------------------------
 filename <- make_filename(2013)
 
-## ---- year_read, echo = TRUE, eval = TRUE--------------------------------
+## ---- year_read, echo = TRUE, eval = TRUE, warning = FALSE---------------
+setwd(system.file("extdata", package = "farspack"))
 subset_2013 <- fars_read(filename)
 
-## ---- years_read, echo = TRUE, eval = TRUE-------------------------------
+## ---- years_read, echo = TRUE, eval = TRUE, warning = FALSE--------------
+setwd(system.file("extdata", package = "farspack"))
 all_years <- fars_read_years(years = list(2013,2014,2015))
 
-## ---- accidents_summary, echo = TRUE, eval = TRUE------------------------
+## ---- accidents_summary, echo = TRUE, eval = TRUE, warning = FALSE-------
+setwd(system.file("extdata", package = "farspack"))
 all_res <- fars_summarize_years( years = 2013:2015)
 
 ## ---- accidents_map, echo = TRUE, eval = TRUE, warning = FALSE-----------
+setwd(system.file("extdata", package = "farspack"))
 map <- fars_map_state(10,2013)
 
